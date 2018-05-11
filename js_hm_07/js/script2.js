196fb5ffa2a08da855953787385b172ba42ca35f
AndrewSlobodianiuk <speshmillo@gmail.com>
2018-05-11 16:27:13 +0300

    js hm ready

--- js_hm_07/js/script2.js
+++ js_hm_07/js/script2.js
@@ -39,30 +39,71 @@ const posts = [
 const wrapper = document.getElementsByClassName("wrapper");
 
 const createPostCard = (urlImg, postTitle, postText, postStats) => {
+  const postStatsArr = [];
+  let flag = 0;
+
+  for (let el in postStats) {
+    postStatsArr.push(postStats[el]);
+  }
+
+  const string = `<ul class="actions post__actions">${postStatsArr.reduce(
+    (acc, x) => {
+      switch (flag) {
+        case 0:
+          flag++;
+          return (
+            acc +
+            `<li class="actions__item">
+  <button class="actions__btn ">
+       <span class="actions__icon actions__icon--like"></span>
+      <span class="actions__count">${x}</span>
+  </button>
+</li>`
+          );
+          break;
+
+        case 1:
+          flag++;
+          return (
+            acc +
+            `<li class="actions__item">
+  <button class="actions__btn ">
+       <span class="actions__icon actions__icon--dislike"></span>
+      <span class="actions__count">${x}</span>
+  </button>
+</li>`
+          );
+          break;
+
+        case 2:
+          flag++;
+          return (
+            acc +
+            `<li class="actions__item">
+  <button class="actions__btn ">
+       <span class="actions__icon actions__icon--fav"></span>
+      <span class="actions__count">${x}</span>
+  </button>
+</li>`
+          );
+          break;
+      }
+    },
+    ""
+  )}</ul>`;
 
   return `<div class="post">
 <img class="post__image" src=${urlImg} alt="post image">
 <h2 class="post__title">${postTitle}</h2>
 <p class="post__text"> ${postText}</p>
-
-<ul class="actions post__actions">
-
-
-<li class="actions__item">
-    <button class="actions__btn ">
-         <span class="actions__icon actions__icon--fav"></span>
-        <span class="actions__count">0</span>
-    </button>
-</li>
-
-
-</ul>
+${string}
 </div>`;
 };
 
-wrapper[0].innerHTML = createPostCard(
-  posts[0].img,
-  posts[0].title,
-  posts[0].text,
-  posts[0].stats
-);
+const makeCards = arr => {
+  return arr.reduce((acc, x) => {
+    return acc + createPostCard(x.img, x.title, x.text, x.stats);
+  }, "");
+};
+
+wrapper[0].innerHTML = makeCards(posts);
