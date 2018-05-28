d1a355a59ae5e7fcb396d82b660bc98ade117cf7
Andrew Slobodianiuk <speshmillo@gmail.com>
2018-05-28 14:37:57 +0300

    ready js 10

--- js_hm_10/js/script.js
+++ js_hm_10/js/script.js
@@ -13,138 +13,231 @@ const inputName = document.querySelector(".name");
 const inputAge = document.querySelector(".age");
 
 const getAllUsers = () => {
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", getAllUsersUrl, true);
-  xhr.send();
+  // const xhr = new XMLHttpRequest();
+  // xhr.open("GET", getAllUsersUrl, true);
+  // xhr.send();
 
-  const stringyHTML = arr => {
-    const string = `${arr.reduce((acc, item) => {
-      return (
-        acc +
-        `<ul><li>${item.name}</li><li>${item.age}</li><li>${item.id}</li></ul>`
-      );
-    }, "")}`;
+  // const stringyHTML = arr => {
+  //   const string = `${arr.reduce((acc, item) => {
+  //     return (
+  //       acc +
+  //       `<ul><li>${item.name}</li><li>${item.age}</li><li>${item.id}</li></ul>`
+  //     );
+  //   }, "")}`;
 
-    const cont = document.querySelector(".container");
-    cont.innerHTML = string;
-  };
+  //   const cont = document.querySelector(".container");
+  //   cont.innerHTML = string;
+  // };
 
-  xhr.addEventListener("load", () => {
-    if (xhr.status < 200 || xhr.status > 299) {
-      console.error(`${xhr.status}: ${xhr.statusText}`);
-    } else {
-      stringyHTML(JSON.parse(xhr.response).data);
-    }
-  });
+  // xhr.addEventListener("load", () => {
+  //   if (xhr.status < 200 || xhr.status > 299) {
+  //     console.error(`${xhr.status}: ${xhr.statusText}`);
+  //   } else {
+  //     stringyHTML(JSON.parse(xhr.response).data);
+  //   }
+  // });
 
-  xhr.addEventListener("error", function() {
-    console.error(`${xhr.status}: ${xhr.responseText}`);
-  });
+  // xhr.addEventListener("error", function() {
+  //   console.error(`${xhr.status}: ${xhr.responseText}`);
+  // });
+
+  fetch(getAllUsersUrl)
+    .then(response => {
+      if (response.ok) return response.json();
+
+      throw new Error(`Error while fetching: ${response.statusText}`);
+    })
+    .then(data => {
+      const stringyHTML = arr => {
+        const string = `${arr.reduce((acc, item) => {
+          return (
+            acc +
+            `<ul><li>${item.name}</li><li>${item.age}</li><li>${
+              item.id
+            }</li></ul>`
+          );
+        }, "")}`;
+
+        const cont = document.querySelector(".container");
+        cont.innerHTML = string;
+      };
+
+      stringyHTML(data.data);
+    });
 };
 
 const getUserById = id => {
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", `${getAllUsersUrl}${id}`, true);
-  xhr.send();
+  // const xhr = new XMLHttpRequest();
+  // xhr.open("GET", `${getAllUsersUrl}${id}`, true);
+  // xhr.send();
 
-  const stringyHTML = obj => {
-    const string = `<ul><li>${obj.name}</li><li>${obj.age}</li><li>${
-      obj.id
-    }</li></ul>`;
+  // const stringyHTML = obj => {
+  //   const string = `<ul><li>${obj.name}</li><li>${obj.age}</li><li>${
+  //     obj.id
+  //   }</li></ul>`;
 
-    const cont = document.querySelector(".container");
-    cont.innerHTML = string;
-  };
+  //   const cont = document.querySelector(".container");
+  //   cont.innerHTML = string;
+  // };
 
-  xhr.addEventListener("load", () => {
-    if (xhr.status < 200 || xhr.status > 299) {
-      console.error(`${xhr.status}: ${xhr.statusText}`);
-    } else {
-      stringyHTML(JSON.parse(xhr.response).data);
-    }
-  });
+  // xhr.addEventListener("load", () => {
+  //   if (xhr.status < 200 || xhr.status > 299) {
+  //     console.error(`${xhr.status}: ${xhr.statusText}`);
+  //   } else {
+  //     stringyHTML(JSON.parse(xhr.response).data);
+  //   }
+  // });
 
-  xhr.addEventListener("error", function() {
-    console.error(`${xhr.status}: ${xhr.responseText}`);
-  });
+  // xhr.addEventListener("error", function() {
+  //   console.error(`${xhr.status}: ${xhr.responseText}`);
+  // });
+
+  fetch(`${getAllUsersUrl}${id}`)
+    .then(response => {
+      if (response.ok) return response.json();
+
+      throw new Error(`PIZDEC ${response.responseText}`);
+    })
+    .then(data => {
+      const stringyHTML = obj => {
+        const string = `<ul><li>${obj.name}</li><li>${obj.age}</li><li>${
+          obj.id
+        }</li></ul>`;
+
+        const cont = document.querySelector(".container");
+        cont.innerHTML = string;
+      };
+
+      stringyHTML(data.data);
+    });
 };
 
 const addUser = (name, age) => {
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", getAllUsersUrl, true);
+  // const xhr = new XMLHttpRequest();
+  // xhr.open("POST", getAllUsersUrl, true);
 
-  xhr.setRequestHeader("Accept", "application/json");
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.send(JSON.stringify({ name: name, age: age }));
+  // xhr.setRequestHeader("Accept", "application/json");
+  // xhr.setRequestHeader("Content-Type", "application/json");
+  // xhr.send(JSON.stringify({ name: name, age: age }));
 
-  xhr.addEventListener("load", () => {
-    if (xhr.status < 200 || xhr.status > 299) {
-      console.error(`${xhr.status}: ${xhr.statusText}`);
-    } else {
-      const data = JSON.parse(xhr.response).data;
+  // xhr.addEventListener("load", () => {
+  //   if (xhr.status < 200 || xhr.status > 299) {
+  //     console.error(`${xhr.status}: ${xhr.statusText}`);
+  //   } else {
+  //     const data = JSON.parse(xhr.response).data;
 
+  //     console.log(
+  //       `Пользователь ${data.name} записан! Возраст ${data.age}, id - ${
+  //         data._id
+  //       }`
+  //     );
+  //   }
+  // });
+
+  // xhr.addEventListener("error", function() {
+  //   console.error(`${xhr.status}: ${xhr.responseText}`);
+  // });
+
+  fetch(getAllUsersUrl, {
+    method: "POST",
+    headers: new Headers({
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    }),
+    body: JSON.stringify({ name: name, age: age })
+  })
+    .then(response => response.json())
+    .then(obj =>
       console.log(
-        `Пользователь ${data.name} записан! Возраст ${data.age}, id - ${
-          data._id
+        `Пользователь ${obj.data.name} записан! Возраст ${obj.data.age}, id - ${
+          obj.data._id
         }`
-      );
-    }
-  });
-
-  xhr.addEventListener("error", function() {
-    console.error(`${xhr.status}: ${xhr.responseText}`);
-  });
+      )
+    );
 };
 
 const removeUser = id => {
-  const xhr = new XMLHttpRequest();
-  xhr.open("DELETE", `${getAllUsersUrl}${id}`, true);
-  xhr.send();
+  // const xhr = new XMLHttpRequest();
+  // xhr.open("DELETE", `${getAllUsersUrl}${id}`, true);
+  // xhr.send();
 
-  xhr.addEventListener("load", () => {
-    if (xhr.status < 200 || xhr.status > 299) {
-      console.error(`${xhr.status}: ${xhr.statusText}`);
-    } else {
-      const data = JSON.parse(xhr.response).data;
-      console.log(data);
+  // xhr.addEventListener("load", () => {
+  //   if (xhr.status < 200 || xhr.status > 299) {
+  //     console.error(`${xhr.status}: ${xhr.statusText}`);
+  //   } else {
+  //     const data = JSON.parse(xhr.response).data;
+  //     console.log(data);
 
+  //     console.log(
+  //       `Пользователь ${data.name} удален! Возраст ${data.age}, id - ${data.id}`
+  //     );
+  //   }
+  // });
+
+  // xhr.addEventListener("error", function() {
+  //   console.error(`${xhr.status}: ${xhr.responseText}`);
+  // });
+
+  fetch(`${getAllUsersUrl}${id}`, {
+    method: "DELETE"
+  })
+    .then(response => response.json())
+    .then(obj =>
       console.log(
-        `Пользователь ${data.name} удален! Возраст ${data.age}, id - ${data.id}`
-      );
-    }
-  });
-
-  xhr.addEventListener("error", function() {
-    console.error(`${xhr.status}: ${xhr.responseText}`);
-  });
+        `Пользователь ${obj.data.name} удален! Возраст ${obj.data.age}, id - ${
+          obj.data._id
+        }`
+      )
+    );
 };
 
 const updateUser = (id, user) => {
-  const xhr = new XMLHttpRequest();
-  xhr.open("PUT", `${getAllUsersUrl}${id}`, true);
+  // const xhr = new XMLHttpRequest();
+  // xhr.open("PUT", `${getAllUsersUrl}${id}`, true);
 
-  xhr.setRequestHeader("Accept", "application/json");
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.send(JSON.stringify(user));
+  // xhr.setRequestHeader("Accept", "application/json");
+  // xhr.setRequestHeader("Content-Type", "application/json");
+  // xhr.send(JSON.stringify(user));
 
-  xhr.addEventListener("load", () => {
-    if (xhr.status < 200 || xhr.status > 299) {
-      console.error(`${xhr.status}: ${xhr.statusText}`);
-    } else {
-      const data = JSON.parse(xhr.response).data;
-      console.log(data);
+  // xhr.addEventListener("load", () => {
+  //   if (xhr.status < 200 || xhr.status > 299) {
+  //     console.error(`${xhr.status}: ${xhr.statusText}`);
+  //   } else {
+  //     const data = JSON.parse(xhr.response).data;
+  //     console.log(data);
 
+  //     console.log(
+  //       `Пользователь ${data.name} успешно обновлен! Возраст ${
+  //         data.age
+  //       }, id - ${data.id}`
+  //     );
+  //   }
+  // });
+
+  // xhr.addEventListener("error", function() {
+  //   console.error(`${xhr.status}: ${xhr.responseText}`);
+  // });
+
+  const request = new Request(`${getAllUsersUrl}${id}`, {
+    method: "PUT",
+    headers: new Headers({
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    }),
+    body: JSON.stringify(user)
+  });
+
+  fetch(request)
+    .then(response => {
+      if (response.ok) return response.json();
+    })
+    .then(obj =>
       console.log(
-        `Пользователь ${data.name} успешно обновлен! Возраст ${
-          data.age
-        }, id - ${data.id}`
-      );
-    }
-  });
-
-  xhr.addEventListener("error", function() {
-    console.error(`${xhr.status}: ${xhr.responseText}`);
-  });
+        `Пользователь ${obj.data.name} обновлен! Возраст ${obj.data.age}, id - ${
+          obj.data._id
+        }`
+      )
+    );
 };
 
 btnEdit.addEventListener("click", () =>
