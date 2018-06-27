8dae4a94513a1caa611829bb55a4c202eb14b50f
AndrewSlobodianiuk <speshmillo@gmail.com>
2018-06-27 13:39:27 +0300

    new

--- js_hm_13/webpack.config.js
+++ js_hm_13/webpack.config.js
@@ -1,9 +1,32 @@
-var path = require("path");
+var path = require('path');
 
 module.exports = {
-  entry: "./src/js/index.js",
+  entry: './src/js/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist/js")
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist/js')
+  },
+
+  module: {
+    rules: [{
+      test: /\.js$/, // Run the loader on all .js files
+      exclude: /node_modules/, // ignore all files in the node_modules folder
+      use: 'jshint-loader'
+    }]
+
+
+
+
+
   }
+
+
+
+
+
+
+
+
+
+
 };
